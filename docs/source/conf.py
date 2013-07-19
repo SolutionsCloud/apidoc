f4e376655d039416f2c0d6bd13080497e9b8ece4
Jérémy Derussé <jeremy@derusse.com>
2013-07-19 20:46:41 +0200

    Fix unicaode in copyright. Remove generated examples

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -42,7 +42,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'ApiDoc'
-copyright = '2013, Jérémy Derussé'
+copyright = '2013, Jeremy Derusse, SFR Business Team'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
