c73653b813b5111235c7756c12dd48fb9cc60303
Jérémy Derussé <jeremy@derusse.com>
2013-08-05 18:12:54 +0200

    Add informations in setup.py

--- setup.py
+++ setup.py
@@ -32,21 +32,34 @@ setup(
     name='ApiDoc',
     version='1.0',
     description='Api Documentation Generator',
-    long_description='''ApiDoc is a documentation generator designe for API built with Python.
-It's developed by Jérémy Derussé and SFR Business Team.
+    long_description='''About
+=====
 
-Full documentation available on http://apidoc.rtfd.org.''',
+ApiDoc is a documentation generator designe for API built with Python.
+
+Full documentation available on https://apidoc.rtfd.org.
+
+Installation
+============
+
+$ pip3 install apidoc''',
     author='Jérémy Derussé',
     author_email='jeremy.derusse@sfr.com',
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 3 - Alpha',
         'Environment :: Console',
         'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Natural Language :: English',
+        'Operating System :: MacOS',
+        'Operating System :: POSIX :: Linux',
         'Programming Language :: Python :: 3',
         'Topic :: Documentation',
         'Topic :: Software Development :: Documentation',
     ],
     platforms=['Any'],
+    license='GPLv3+',
+    url='http://solutionscloud.github.io/apidoc/',
     packages=find_packages(exclude=['tests', 'tests.*', 'example', 'example.*', 'docs', 'docs.*']),
     entry_points={
         'console_scripts': [
