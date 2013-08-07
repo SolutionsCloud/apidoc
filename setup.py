bca09785d2a50845afe8ba4c392a05ab26db5b0c
Jérémy Derussé <jeremy@derusse.com>
2013-08-07 16:02:46 +0200

    Fixes #6: Fix bug when running setup in a different cwd

--- setup.py
+++ setup.py
@@ -39,32 +39,59 @@ class Resource(Command):
         pass
 
     def run(self):
+        resource_dir = os.path.realpath(
+            os.path.join(
+                os.path.dirname(__file__),
+                'apidoc',
+                'template',
+                'resource'
+            )
+        )
+
+        resource_src_dir = os.path.realpath(os.path.join(resource_dir, 'src'))
+        resource_src_js_dir = os.path.realpath(os.path.join(resource_src_dir, 'js'))
+        resource_src_css_dir = os.path.realpath(os.path.join(resource_src_dir, 'css'))
+        resource_src_less_dir = os.path.realpath(os.path.join(resource_src_dir, 'less'))
+        resource_js_dir = os.path.realpath(os.path.join(resource_dir, 'js'))
+        resource_css_dir = os.path.realpath(os.path.join(resource_dir, 'css'))
+
         os.system("wget --post-data='css=%s&js=%s' -O '/tmp/bootstrap.zip' %s" % (
             '["reset.less","scaffolding.less","grid.less","layouts.less","type.less","code.less","tables.less","forms.less","navs.less","navbar.less","tooltip.less","popovers.less","modals.less","wells.less","close.less","utilities.less","component-animations.less","responsive-utilities.less","responsive-767px-max.less","responsive-768px-979px.less","responsive-1200px-min.less","responsive-navbar.less"]',
             '["bootstrap-transition.js","bootstrap-modal.js","bootstrap-scrollspy.js","bootstrap-tooltip.js","bootstrap-popover.js","bootstrap-affix.js"]',
             'http://bootstrap.herokuapp.com'
         ))
 
+        assert os.path.exists('/tmp/bootstrap.zip'), 'Downloaded bootstrap zip not found'
+
         try:
             os.system('unzip /tmp/bootstrap.zip -d /tmp/apidoc-bootstrap')
-            shutil.move('/tmp/apidoc-bootstrap/js/bootstrap.min.js', 'apidoc/template/resource/src/js/bootstrap.min.js')
-            shutil.move('/tmp/apidoc-bootstrap/css/bootstrap.min.css', 'apidoc/template/resource/src/css/bootstrap.min.css')
+
+            shutil.move('/tmp/apidoc-bootstrap/js/bootstrap.min.js', '%s/bootstrap.min.js' % resource_src_js_dir)
+            shutil.move('/tmp/apidoc-bootstrap/css/bootstrap.min.css', '%s/bootstrap.min.css' % resource_src_css_dir)
         finally:
             os.remove('/tmp/bootstrap.zip')
             shutil.rmtree('/tmp/apidoc-bootstrap')
 
-        os.system('wget -O "%s" "%s"' % ('apidoc/template/resource/src/js/mousetrap.min.js', 'http://cdn.craig.is/js/mousetrap/mousetrap.min.js'))
-        os.system('wget -O "%s" "%s"' % ('apidoc/template/resource/src/js/jquery.min.js', 'http://code.jquery.com/jquery-2.0.3.min.js'))
+        os.system('wget -O "%s" "%s"' % ('%s/jquery.min.js' % resource_src_js_dir, 'http://code.jquery.com/jquery-2.0.3.min.js'))
+        assert os.path.exists('%s/mousetrap.min.js' % resource_src_js_dir), 'Downloaded jquery file not found'
 
-        os.system('lessc -x "apidoc/template/resource/src/less/apidoc.less" "apidoc/template/resource/src/css/apidoc.css"')
+        os.system('wget -O "%s" "%s"' % ('%s/mousetrap.min.js' % resource_src_js_dir, 'http://cdn.craig.is/js/mousetrap/mousetrap.min.js'))
+        assert os.path.exists('%s/mousetrap.min.js' % resource_src_js_dir), 'Downloaded mousetrap file not found'
 
-        for folder in  ['apidoc/template/resource/css', 'apidoc/template/resource/js']:
+        os.system('lessc -x "%s/apidoc.less" "%s/apidoc.css"' % (resource_src_less_dir, resource_src_css_dir))
+
+        for folder in  [resource_css_dir, resource_js_dir]:
             if not os.path.exists(folder):
                 os.makedirs(folder)
 
-        os.system('./setup.py minify_css --sources "apidoc/template/resource/src/css/*.css apidoc/template/resource/css/font.css" --output "apidoc/template/resource/css/combined.css"')
-        os.system('./setup.py minify_css --sources "apidoc/template/resource/src/css/*.css apidoc/template/resource/css/font-embedded.css" --output "apidoc/template/resource/css/combined-embedded.css"')
-        os.system('./setup.py minify_js --sources "apidoc/template/resource/src/js/*.js" --output "apidoc/template/resource/js/combined.js"')
+        os.system('/usr/bin/env python3 %s minify_css --sources "%s/*.css %s/font.css" --output "%s/combined.css"' % (__file__, resource_src_css_dir, resource_css_dir, resource_css_dir))
+        assert os.path.exists('%s/combined.css' % resource_css_dir), 'Combined css file not found'
+
+        os.system('/usr/bin/env python3 %s minify_css --sources "%s/*.css %s/font-embedded.css" --output "%s/combined-embedded.css"' % (__file__, resource_src_css_dir, resource_css_dir, resource_css_dir))
+        assert os.path.exists('%s/combined-embedded.css' % resource_css_dir), 'Combined embedded css file not found'
+
+        os.system('/usr/bin/env python3 %s minify_js --sources "%s/*.js" --output "%s/combined.js"' % (__file__, resource_src_js_dir, resource_js_dir))
+        assert os.path.exists('%s/combined.js' % resource_js_dir), 'Combined js file not found'
 
 
 
