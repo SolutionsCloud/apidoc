47aed4032c779c280780379f0caef5444e033375
requires.io <support@requires.io>
2013-12-17 02:44:54 -0800

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -61,10 +61,10 @@ setup(
         'settings/logging.yml',
     ]},
     install_requires=requirements,
-    tests_require=['pytest==2.4.2', 'mock==1.0.1'],
+    tests_require=['pytest==2.5.1', 'mock==1.0.1'],
     extras_require={
-        'ci': ['flake8==2.1.0', 'behave==1.2.3', 'coverage==3.7', 'coveralls==0.3', 'mock==1.0.1', 'pytest==2.4.2'],
-        'contribute': ['flake8==2.1.0', 'behave==1.2.3', 'coverage==3.7', 'mock==1.0.1', 'pytest==2.4.2', 'Sphinx==1.2', 'sphinx_rtd_themen==0.1.5', 'yuicompressor==2.4.8'],
+        'ci': ['flake8==2.1.0', 'behave==1.2.3', 'coverage==3.7.1', 'coveralls==0.3', 'mock==1.0.1', 'pytest==2.5.1'],
+        'contribute': ['flake8==2.1.0', 'behave==1.2.3', 'coverage==3.7.1', 'mock==1.0.1', 'pytest==2.5.1', 'Sphinx==1.2', 'sphinx_rtd_themen==0.1.5', 'yuicompressor==2.4.8'],
     },
     cmdclass={
         'test': ApiDocTest,
